d4412ca7c5851c26a124c84c0afe6c5766478711
TenTenTne <kikacat19@gmail.com>
2025-06-11 13:56:36 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,15 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/index'
+import AdsenseCalc from './pages/adsense-calc'
+// 앞으로 여기에 다른 기능도 추가됨
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/adsense-calc" element={<AdsenseCalc />} />
+    </Routes>
+  )
+}
 
