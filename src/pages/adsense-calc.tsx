90eb66af9407e4200d3a2052600d3f5d5f67c1f0
TenTenTne <kikacat19@gmail.com>
2025-06-11 13:58:12 +0900

    Update adsense-calc.tsx

--- src/pages/adsense-calc.tsx
+++ src/pages/adsense-calc.tsx
@@ -1,1 +1,12 @@
+import React from "react"
+
+export default function AdsenseCalc() {
+  return (
+    <div style={{ padding: '2rem', maxWidth: 700, margin: 'auto' }}>
+      <h1>📊 AdSense 수익 계산기</h1>
+      {/* 👉 여기 안에 계산기 폼 + 로직 붙여넣으면 됨 */}
+      <p>이곳에 계산기 기능 UI를 추가할 수 있습니다.</p>
+    </div>
+  )
+}
 
